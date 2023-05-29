7ffc281a44740326fb066e45e6a59b336d697ada
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-05-29 14:08:12 +0100

    Add listener for mobile
    Resize the Computers responsive for mobile devices

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { Canvas } from '@react-three/fiber';
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
 
     const computer = useGLTF('./desktop_pc/scene.gltf');
 
@@ -22,15 +22,35 @@ const Computers = () => {
             />
             <primitive
                 object={computer.scene}
-                scale={0.75}
-                position={[0, -3.25, -1.5]}
+                scale={isMobile ? 0.7 : 0.75}
+                position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
                 rotation={[-0.01, -0.2, -0.1]}
             />
         </mesh>
-    )
-}
+    );
+};
 
 const ComputerCanvas = () => {
+    const [isMobile, setisMobile] = useState(false);
+
+    useEffect(() => {
+
+        //Add a listener for changes to the screen size
+        const mediaQuery = window.matchMedia('(max-width: 500px)')
+        // Set the initial value of the 'isMobile' state variable
+        setisMobile(mediaQuery.matches);
+        //Define a callback function to handle changes to the media query
+        const handleMediaQueryChange = (e) => {
+            setisMobile(e.matches);
+        }
+        // Add the callback function as a listener for changes to the media query
+        mediaQuery.addEventListener('change', handleMediaQueryChange);
+        // Remove the listener when the component is unmounted
+        return () => {
+            mediaQuery.removeEventListener('change', handleMediaQueryChange)
+        }
+    }, []);
+
     return (
         <Canvas
             shadows
@@ -43,11 +63,11 @@ const ComputerCanvas = () => {
                     maxPolarAngle={Math.PI / 2}
                     minPolarAngle={Math.PI / 2}
                 />
-                <Computers />
+                <Computers isMobile={isMobile} />
             </Suspense>
             <Preload all />
         </Canvas>
-    )
-}
+    );
+};
 
 export default ComputerCanvas;
