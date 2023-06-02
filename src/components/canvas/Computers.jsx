1910cbd5adb40750bee0c176479ccfd0e89ca98a
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-06-03 00:15:58 +0100

    Creating 3d computer image

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,6 +20,7 @@ const Computers = ({ isMobile }) => {
                 castShadow
                 shadow-mapSize={1024}
             />
+            <pointLight intensity={1} />
             <primitive
                 object={computer.scene}
                 scale={isMobile ? 0.7 : 0.75}
@@ -31,17 +32,17 @@ const Computers = ({ isMobile }) => {
 };
 
 const ComputerCanvas = () => {
-    const [isMobile, setisMobile] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
 
         //Add a listener for changes to the screen size
         const mediaQuery = window.matchMedia('(max-width: 500px)')
         // Set the initial value of the 'isMobile' state variable
-        setisMobile(mediaQuery.matches);
+        setIsMobile(mediaQuery.matches);
         //Define a callback function to handle changes to the media query
         const handleMediaQueryChange = (e) => {
-            setisMobile(e.matches);
+            setIsMobile(e.matches);
         }
         // Add the callback function as a listener for changes to the media query
         mediaQuery.addEventListener('change', handleMediaQueryChange);
@@ -53,7 +54,9 @@ const ComputerCanvas = () => {
 
     return (
         <Canvas
+            frameloop='demand'
             shadows
+            dpr={[1, 2]}
             camera={{ position: [20, 3, 5], fov: 25 }}
             gl={{ preserveDrawingBuffer: true }}
         >
