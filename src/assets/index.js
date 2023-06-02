657291c44e980a2e9fd015b79a9d4af92c0d113a
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-06-03 00:19:22 +0100

    Update index js file in assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,17 +21,19 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import enterprise from "./company/enterprise.png";
+import europcar from "./company/europcar.jpg";
+import tsw from "./company/TSW.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import p1 from "./p1.png";
+import p2 from "./p2.png";
+import p3 from "./p3.png";
+
+import ivo from "./ivo.png";
 
 export {
   logo,
+  ivo,
   backend,
   creator,
   mobile,
@@ -52,11 +54,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+  enterprise,
+  europcar,
+  tsw,
+  p1,
+  p2,
+  p3,
 };
