aaf42b7fa065c355be78cf98f2f1fc6d9209d2ca
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-06-03 00:13:26 +0100

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
-
 const App = () => {
 
     return (
@@ -15,7 +14,7 @@ const App = () => {
                 <Experience />
                 <Tech />
                 <Works />
-                <Feedbacks />
+                {/* <Feedbacks />  */}
                 <div className="relative z-0">
                     <Contact />
                     <StarsCanvas />
@@ -25,4 +24,4 @@ const App = () => {
     )
 }
 
-export default App
+export default App;
