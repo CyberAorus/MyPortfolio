4fe62f3e0b064355c0a6984c60ece10c33b9a4dc
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-06-03 00:18:25 +0100

    Add Work and Education section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+
+import { motion } from "framer-motion";
+import { fadeIn, textVariant } from '../utils/motion';
+import { projects } from '../constants';
+import { styles } from '../styles';
+
+import { SectionWrapper } from '../hoc';
+import ProjectCard from '../common/ProjectCard';
+
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
+    return (
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={styles.sectionSubText}>My Work</p>
+                <h2 className={styles.sectionHeadText}>Projects</h2>
+            </motion.div>
+
+            <div className='w-full flex'>
+                <motion.p
+                    variants={fadeIn("", "", 0.1, 1)}
+                    className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+                >
+                    The projects listed below were built with the aim of enhancing educational experiences.
+                    It is worth mentioning that several of these projects were developed while I was enrolled in my courses.
+                    
+                </motion.p>
+            </div>
+
+            <div className='mt20 flex flex-wrap gap-7'>
+                {projects.map((project, index) => (
+                    <ProjectCard key={`project-${index}`} index={index} {...project} />
+                ))}
+            </div>
+        </>
+    )
 }
 
-export default Works
+export default SectionWrapper(Works, 'work');
