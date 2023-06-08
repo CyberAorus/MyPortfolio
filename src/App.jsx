965f77bcbf7365d8c049e9b387d3ee71b53f3ffc
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-06-08 20:59:36 +0100

    remove Hero css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works, StarsCanvas } from './components';
 
 const App = () => {
 
@@ -12,9 +12,8 @@ const App = () => {
                 </div>
                 <About />
                 <Experience />
-                <Tech />
                 <Works />
-                {/* <Feedbacks />  */}
+                <Feedbacks />
                 <div className="relative z-0">
                     <Contact />
                     <StarsCanvas />
