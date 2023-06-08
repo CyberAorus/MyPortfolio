6c3c44ee1095e69edece5887abe0d09f1d5556c2
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-06-08 20:55:16 +0100

    Update Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,29 @@
 import { motion } from 'framer-motion';
-
 import { styles } from '../styles';
-import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
     return (
         <section className='relative w-full h-screen mx-auto'>
-            <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start  gap-5`}>
+            <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
                 <div className='flex flex-col justify-center items-center mt-5'>
                     <div className='w-5 h-5 rounded-full bg-[#915eff]' />
                     <div className='w-1 sm:h-80 h-40 violet-gradient' />
                 </div>
                 <div>
-                    <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className='text-[#915eff]'>Ivo</span></h1>
-                    <p className={`${styles.heroSubText} mt-2 text-white-100`}>Welcome to my protfolio</p>
+                    <h1 className={`${styles.heroHeadText}`}>Hello, I'm <span className='text-[#915eff]'>Ivo</span></h1>
+                    <p className={`${styles.heroSubText} mt-2 text-white-100`}>Passionate Web Developer Constantly Evolving Through Hands-On Coding</p>
+                </div>
+                <div className='container absolute inset-x-0'>
+                    <div className='box'>
+                        <div className='spin-container'>
+                            <div className='shape'>
+                                <div className='bd'></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <ComputersCanvas />
+
 
             <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
                 <a href="#about">
@@ -39,4 +46,4 @@ const Hero = () => {
     )
 }
 
-export default Hero
+export default Hero;
