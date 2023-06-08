3b8263b61628488b429a54c2ac9a77b6521c2c11
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-06-08 21:01:09 +0100

    Update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,22 +5,19 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-import { ivo } from "../assets";
 import { about } from "../constants";
 
 import ServiceCard from "../common/ServiceCard";
 
-
 const About = () => {
-    
+
     return (
-        <>
+        <>  
             <motion.div variants={textVariant()}>
                 <p className={`${styles.sectionSubText} text-center`}>Introduction</p>
                 <h2 className={`${styles.sectionHeadText} text-center`}>Overview</h2>
             </motion.div>
 
-            <img src={ivo} alt="ivo" className="xs:w-[250px] w-full" />
             <motion.p
                 variants={fadeIn("", "", 0.1, 1)}
                 className='mt-4 text-secondary text-[16px] max-w-3xl leading-[30px]'
