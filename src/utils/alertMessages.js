e72bce095657a645bac2d02ad476493d5292916b
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-06-11 19:10:14 +0100

    Create alertMessages helper

---
+++ src/utils/alertMessages.js
@@ -0,0 +1,22 @@
+import { toast } from "react-toastify";
+export const msgSent = () => toast.success('Thank you for getting in touch!', {
+    position: "bottom-left",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+});
+
+export const msgError = () => toast.error('Something went wrong! Please try again!', {
+    position: "bottom-left",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+});
