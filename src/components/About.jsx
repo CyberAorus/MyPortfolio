5f248064c0d7461a4560d5e1468e257ccb38fecc
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-10-01 15:38:08 +0100

    text centered About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
 
             <motion.p
                 variants={fadeIn("", "", 0.1, 1)}
-                className='mt-4 text-secondary text-[16px] max-w-3xl leading-[30px]'
+                className='mt-4 text-secondary text-[16px] leading-[26px]'
             >
                 {about.info}
             </motion.p>
