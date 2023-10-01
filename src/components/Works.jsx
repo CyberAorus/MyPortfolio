a18fa42d8932651be1b837fcb3ae6644c6cbef7d
IVAYLO MARKOV <ivo.markoff@gmail.com>
2023-10-01 15:37:40 +0100

    Remove p text from the section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -13,22 +13,11 @@ const Works = () => {
     return (
         <>
             <motion.div variants={textVariant()}>
-                <p className={styles.sectionSubText}>My Work</p>
-                <h2 className={styles.sectionHeadText}>Projects</h2>
+                <p className={`${styles.sectionSubText} text-center`}>My Work</p>
+                <h2 className={`${styles.sectionHeadText} text-center`}>Projects</h2>
             </motion.div>
 
-            <div className='w-full flex'>
-                <motion.p
-                    variants={fadeIn("", "", 0.1, 1)}
-                    className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
-                >
-                    The projects listed below were built with the aim of enhancing educational experiences.
-                    Couple of these projects were developed while I was enrolled in my courses.
-                    
-                </motion.p>
-            </div>
-
-            <div className='mt20 flex flex-wrap gap-7'>
+            <div className='mt-20 flex flex-wrap gap-7'>
                 {projects.map((project, index) => (
                     <ProjectCard key={`project-${index}`} index={index} {...project} />
                 ))}
